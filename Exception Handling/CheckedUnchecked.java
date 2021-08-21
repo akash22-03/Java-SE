bc8bc47c11eb26652753cf53181c2b186687e0ea
Akash Dash <56639516+akash22-03@users.noreply.github.com>
2021-08-21 19:41:15 +0530

    Exception Handling

---
+++ Exception Handling/CheckedUnchecked.java
@@ -0,0 +1,36 @@
+package ExceptionHandling;
+
+class LowBalanceException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+	
+    public String toString()
+    {
+        return "Balance Should not be less than 5000";
+    }
+}
+
+public class CheckedUnchecked 
+{
+    static void fun1()
+    {
+        try
+        {
+            throw new LowBalanceException();
+        }
+        catch(LowBalanceException e)
+        {
+            System.out.println(e);
+        }
+    }
+    
+    static void fun2(){
+        fun1();
+    }
+    static void fun3(){
+        fun2();
+    }
+    public static void main(String[] args) {
+       fun3();
+    }   
+}
