2758e3b0694d8ef3df8460a9c1bbd4fa857888a4
Akash Dash <56639516+akash22-03@users.noreply.github.com>
2021-08-23 12:29:53 +0530

    Annotations Basic

---
+++ Annotations and JavaDoc/AnnotationsPrac.java
@@ -0,0 +1,39 @@
+package JavaDocPrac;
+
+class A
+{
+    public void display()
+    {
+        
+    }
+}
+
+class B extends A
+{
+    @Override
+    public void display()
+    {
+        
+    }
+    @Deprecated
+    public void show()
+    {
+        
+    }
+    
+}
+
+public class AnnotationsPrac
+{
+    
+    public static void main(String[] args) 
+    {
+        int i;
+        @SuppressWarnings("deprecation")
+        
+        B b=new B();
+  
+        b.show();
+    }
+    
+}
