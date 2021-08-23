c6046b25d82bbe40f14fbf4a325bdc0966092173
Akash Dash <56639516+akash22-03@users.noreply.github.com>
2021-08-23 12:30:28 +0530

    User Defined Annotations

---
+++ Annotations and JavaDoc/UserDefAnno.java
@@ -0,0 +1,26 @@
+package JavaDocPrac;
+
+import java.lang.annotation.Annotation;
+
+@interface MyAnno
+{
+  String name();
+  String project();
+  String date() default "today";
+  String version() default "13";
+}
+
+@MyAnno(name="Ajay",project="Bank")
+public class UserDefAnno 
+{
+  //@MyAnno(name="Ajay")
+  int data;
+  
+  //@MyAnno(name="Ajay")
+  public static void main(String[] args) 
+  {
+      //@MyAnno(name="Ajay")
+      int x;
+  }
+  
+}
