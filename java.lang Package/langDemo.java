d3b925fee1cece44d6f363771c0feb28b4c27919
Akash Dash <56639516+akash22-03@users.noreply.github.com>
2021-08-24 09:03:52 +0530

    java.lang Classes

---
+++ java.lang Package/langDemo.java
@@ -0,0 +1,30 @@
+package JavaLangPackage;
+
+import java.lang.*;
+
+class MyObject
+{
+    public String toString()
+    {
+        return "My Object";
+    } 
+    public int hashCode()
+    {
+        return 100;
+    }
+    public boolean equals(Object o)
+    {
+        return this.hashCode()==o.hashCode();
+    }
+}
+
+public class langDemo 
+{
+    public static void main(String[] args) 
+    {
+        MyObject o1=new MyObject();
+        MyObject o2=new MyObject();
+        
+        System.out.println(o1.equals(o2));
+    }    
+}
